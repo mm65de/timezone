3be1971ff3dc795afadaafc78d10c892ddfc419d
Markus Mueller <markus.mueller@nexusgroup.com>
2025-09-05 22:41:58 +0200

    Using a Logger to make SonarQube happy.

--- src/test/java/de/mm65/github/timezone/VeryBasicTest.java
+++ src/test/java/de/mm65/github/timezone/VeryBasicTest.java
@@ -1,47 +1,53 @@
 package de.mm65.github.timezone;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
+
 
 class VeryBasicTest {
 
-    final static long MILLIS_PER_SECOND = 1000;
-    final static long MILLIS_PER_MINUTE = 60 * MILLIS_PER_SECOND;
-    final static long MILLIS_PER_HOUR = 60 * MILLIS_PER_MINUTE;
-    final static long MILLIS_PER_DAY = 24 * MILLIS_PER_HOUR;
+    private static final Logger LOGGER = LogManager.getLogger();
+
+
+    static final long MILLIS_PER_SECOND = 1000;
+    static final long MILLIS_PER_MINUTE = 60 * MILLIS_PER_SECOND;
+    static final long MILLIS_PER_HOUR = 60 * MILLIS_PER_MINUTE;
+    static final long MILLIS_PER_DAY = 24 * MILLIS_PER_HOUR;
 
     @Test
     void veryBasicAndPartlyInteractiveTest() {
         final long millisNow = System.currentTimeMillis();
         long remainingMillis = millisNow;
         final long days = remainingMillis / MILLIS_PER_DAY;
-        remainingMillis -=  days * MILLIS_PER_DAY;
+        remainingMillis -= days * MILLIS_PER_DAY;
         final long hours = remainingMillis / MILLIS_PER_HOUR;
-        remainingMillis -=  hours * MILLIS_PER_HOUR;
+        remainingMillis -= hours * MILLIS_PER_HOUR;
         final long minutes = remainingMillis / MILLIS_PER_MINUTE;
-        remainingMillis -=  minutes * MILLIS_PER_MINUTE;
+        remainingMillis -= minutes * MILLIS_PER_MINUTE;
         final long seconds = remainingMillis / MILLIS_PER_SECOND;
-        remainingMillis -=  seconds * MILLIS_PER_SECOND;
+        remainingMillis -= seconds * MILLIS_PER_SECOND;
 
         // How to show timezones:
         // X -> "Z", z -> "GMT", Z -> "+0000"
         final DateFormat gmtDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS z");
         gmtDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
 
-        System.out.println("veryBasicAndPartlyInteractiveTest():");
-        System.out.println("------------------------------------");
-        System.out.println("                   millis = " + millisNow);
-        System.out.println("         new Date(millis) = " + new Date(millisNow));
-        System.out.println("        DateFormat as GMT = " + gmtDateFormat.format(new Date(millisNow)));
-        System.out.println("    days | h:m:s | millis = "
-                + days + " | " + hours + ":" + minutes + ":" + seconds + " | " + remainingMillis);
-        System.out.println();
+        LOGGER.info("veryBasicAndPartlyInteractiveTest():");
+        LOGGER.info("------------------------------------");
+        LOGGER.info("                   millis = {}", millisNow);
+        LOGGER.info("         new Date(millis) = {}", new Date(millisNow));
+        LOGGER.info("        DateFormat as GMT = {}", gmtDateFormat.format(new Date(millisNow)));
+        LOGGER.info("    days | h:m:s | millis = {} | {}:{}:{} | {}",
+                days, hours, minutes, seconds, remainingMillis);
+        LOGGER.info("");
 
         // Check the GMT hour against the hour calculated from the milliseconds:
         final DateFormat gmtHour = new SimpleDateFormat("HH");
@@ -54,41 +60,44 @@ class VeryBasicTest {
     void aNewDateInitializesWithGmtMilliseconds() {
         final long millis = System.currentTimeMillis();
         final Date date = new Date();
-        System.out.println("aNewDateInitializesWithGmtMilliseconds():");
-        System.out.println("-----------------------------------------");
-        System.out.println("            System.currentTimeMillis() = " + millis);
-        System.out.println("                  new Date().getTime() = " + date.getTime());
-        System.out.println("    currentTimeMillis - date.getTime() = " +  (millis - date.getTime()));
-        System.out.println();
+        LOGGER.info("aNewDateInitializesWithGmtMilliseconds():");
+        LOGGER.info("-----------------------------------------");
+        LOGGER.info("            System.currentTimeMillis() = {}", millis);
+        LOGGER.info("                  new Date().getTime() = {}", date.getTime());
+        LOGGER.info("    currentTimeMillis - date.getTime() = {}", millis - date.getTime());
+        LOGGER.info("");
         assertEquals(millis, date.getTime(), 1);
     }
 
+    /**
+     * <b>INFO:</b>
+     * <p>How to find valid TimeZone IDs:</p>
+     * <code>Arrays.asList(TimeZone.getAvailableIDs()).stream().forEach(tz -> LOGGER.info(tz));</code>
+     */
     @Test
     void timezonesDoNotInfluenceSystemCurrentMillis() {
         final double MAX_DELTA = 50;
-        // How to find valid TimeZone IDs:
-        //Arrays.asList(TimeZone.getAvailableIDs()).stream().forEach(tz -> System.out.println(tz));
 
-        System.out.println("timezonesDoNotInfluenceSystemCurrentMillis():");
-        System.out.println("---------------------------------------------");
+        LOGGER.info("timezonesDoNotInfluenceSystemCurrentMillis():");
+        LOGGER.info("---------------------------------------------");
 
         final TimeZone tzDefaultToBeRestored = TimeZone.getDefault();
         final long millis1 = System.currentTimeMillis();
 
         TimeZone.setDefault(TimeZone.getTimeZone("WET"));
-        System.out.println(new Date());
+        LOGGER.info(new Date());
         final long millis2 = System.currentTimeMillis();
 
         TimeZone.setDefault(TimeZone.getTimeZone("CET"));
-        System.out.println(new Date());
+        LOGGER.info(new Date());
         final long millis3 = System.currentTimeMillis();
 
         TimeZone.setDefault(TimeZone.getTimeZone("EET"));
-        System.out.println(new Date());
+        LOGGER.info(new Date());
         final long millis4 = System.currentTimeMillis();
 
         TimeZone.setDefault(tzDefaultToBeRestored);
-        System.out.println(new Date());
+        LOGGER.info(new Date());
         final long millis5 = System.currentTimeMillis();
 
         assertEquals(millis1, millis2, MAX_DELTA);
@@ -96,12 +105,9 @@ class VeryBasicTest {
         assertEquals(millis3, millis4, MAX_DELTA);
         assertEquals(millis4, millis5, MAX_DELTA);
 
-        System.out.println("Deltas have been: "
-                + (millis2 - millis1) + ", "
-                + (millis3 - millis2) + ", "
-                + (millis4 - millis3) + ", "
-                + (millis5 - millis4) + "." );
-        System.out.println();
+        LOGGER.info("Deltas have been: {}, {}, {}, {}",
+                millis2 - millis1, millis3 - millis2, millis4 - millis3, millis5 - millis4);
+        LOGGER.info("");
     }
 
 }
